5084795a04cd1f9319687616cdbff31072651637
DTK-18 <120298180+hodinhtuankiet@users.noreply.github.com>
2023-12-24 23:36:41 +0700

    AppBar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,17 +1,24 @@
 import Box from '@mui/material/Box'
 import ModeSelect from '~/components/ModeSelect'
-
+import AppsIcon from '@mui/icons-material/Apps'
 function AppBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.light',
       width: '100%',
       // height: { theme.trello.}
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      // center vertical
+      alignItems: 'center',
+      justifyContent: 'space-between'
     }}>
-      <ModeSelect/>
+      <Box>
+        <AppsIcon sx={ { color: 'primary.main' } }/>
+      Trello
+      </Box>
+      <Box>
+        <ModeSelect/>
+      </Box>
     </Box>
   )
 }
