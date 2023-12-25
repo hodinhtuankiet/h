687142b5a21e457518e2473081cdb1edb51dcd38
DTK-18 <120298180+hodinhtuankiet@users.noreply.github.com>
2023-12-25 22:58:46 +0700

    Done 60%

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,9 +1,13 @@
 import Box from '@mui/material/Box'
 import ModeSelect from '~/components/ModeSelect'
 import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as trelloLogo } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import Workspaces from './Menus/Workspaces'
 function AppBar() {
   return (
-    <Box sx={{
+    <Box px={2} sx={{
       width: '100%',
       // height: { theme.trello.}
       height: (theme) => theme.trello.appBarHeight,
@@ -12,10 +16,15 @@ function AppBar() {
       alignItems: 'center',
       justifyContent: 'space-between'
     }}>
-      <Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: '2' }}>
         <AppsIcon sx={ { color: 'primary.main' } }/>
-      Trello
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: '2' }}>
+          <SvgIcon component={trelloLogo} inheritViewBox sx={ { color: 'primary.main' } } />
+          <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>Trello </Typography>
+        </Box>
+        <Workspaces/>
       </Box>
+
       <Box>
         <ModeSelect/>
       </Box>
