871a64805a229240ace26804e776b2a2fd46420c
DTK-18 <120298180+hodinhtuankiet@users.noreply.github.com>
2024-01-02 18:58:21 +0700

    Footer & Column Title

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -1,16 +1,134 @@
+import * as React from 'react'
+import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import theme from '~/theme'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ContentCut from '@mui/icons-material/ContentCut'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Divider from '@mui/material/Divider'
+import Cloud from '@mui/icons-material/Cloud'
+import Tooltip from '@mui/material/Tooltip'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import AddCardIcon from '@mui/icons-material/AddCard'
+import DragHandleIcon from '@mui/icons-material/DragHandle'
+import { ContentCopy, ContentPaste } from '@mui/icons-material'
+
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTER_HEIGHT = '56px'
 
 function BoardContent() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   return (
     <Box sx={{
       width: '100%',
       // height: { theme.trello.}
-      height: (theme) => `calc(100vh - ${theme.trello.boardBarHeight} - ${theme.trello.appBarHeight})`,
-      alignItems: 'center',
+      height: (theme) => theme.trello.boardContentHeight,
       display: 'flex',
       backgroundColor: theme.colorSchemes.dark.palette.primary.main
-    }}>Box 3</Box>
+    }}>
+      {/* Column */}
+      <Box sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        backgroundColor: (theme) => (theme.colorSchemes.light.palette.primary.main),
+        ml: 2,
+        borderRadius: '6px'
+      }}>
+        <Box
+          sx={{
+            height: COLUMN_HEADER_HEIGHT,
+            p: 2,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}>
+          <Typography variant='h7'
+            sx={{
+              fontWeight: 'bold',
+              cursor: 'pointer'
+            }}>Column Title</Typography>
+          <Box>
+            <Tooltip>
+              <ExpandMoreIcon
+                sx={{ color: 'text.primary', cursor: 'pointer' }}
+                id="basic-button-workspaces"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+              />
+            </Tooltip>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button-workspaces'
+              }}
+            >
+              <MenuItem>
+                <ListItemIcon><AddCardIcon fontSize="small" /></ListItemIcon>
+                <ListItemText>Add New Card</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon><ContentCut fontSize="small" /></ListItemIcon>
+                <ListItemText>Cut</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon><ContentCopy fontSize="small" /></ListItemIcon>
+                <ListItemText>Copy</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon><ContentPaste fontSize="small" /></ListItemIcon>
+                <ListItemText>Paste</ListItemText>
+              </MenuItem>
+              <Divider />
+              <MenuItem>
+                <ListItemIcon><DeleteForeverIcon fontSize="small" /></ListItemIcon>
+                <ListItemText>Remove this column</ListItemText>
+              </MenuItem>
+              <Divider />
+              <MenuItem>
+                <ListItemIcon><Cloud fontSize="small" /></ListItemIcon>
+                <ListItemText>Archive this column</ListItemText>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Box>
+        <Box
+          sx={{ }}>
+            List Card
+        </Box>
+        <Box
+          sx={{
+            height: COLUMN_FOOTER_HEIGHT,
+            p: 2,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <Button sx={{ color: 'text.primary' }} startIcon={<AddCardIcon />}>Add New Card</Button>
+          <Tooltip title="Drag to move ">
+            <DragHandleIcon sx={{ cursor: 'pointer'}}/>
+          </Tooltip>
+        </Box>
+
+      </Box>
+
+    </Box>
   )
 }
 
